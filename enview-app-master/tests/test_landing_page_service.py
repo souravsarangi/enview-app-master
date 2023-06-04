6ceda4ff9473fc1021817a00119deaa6b02235f9
souravsarangi <souravmothers153@gmail.com>
2023-06-05 02:44:10 +0530

    Specs: Create an application for adding events related to vehicle safety and generate alerts as per safety regulations Tools: python3, docker, flask, postman, pytest Author: Sourav Sarangi

--- enview-app-master/tests/test_landing_page_service.py
+++
@@ -1,9 +0,0 @@
-from enview_app.landing_page_service import convert_to_fr
-
-
-def test_convert_to_fr():
-    temp_Cs = [-40, 0, 100]
-    temp_Fs = [-40, 32, 212]
-
-    for temp_C, temp_F in zip(temp_Cs, temp_Fs):
-        assert convert_to_fr(temp_C) == temp_F
